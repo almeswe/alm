ffff6687a8a9c1a9389bb0c60afa502162b8c202
Almeswe <52577119+Almeswe@users.noreply.github.com>
2020-12-22 18:10:20 +0300

    Update Preprocessor.cs

--- Alm/Alm.Core/Alm.Core.SyntaxAnalysis/Preprocessor.cs
+++ Alm/Alm.Core/Alm.Core.SyntaxAnalysis/Preprocessor.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace alm.Core.SyntaxAnalysis
 {
     public sealed class Preprocessor
-    { 
+    {
     }
 }

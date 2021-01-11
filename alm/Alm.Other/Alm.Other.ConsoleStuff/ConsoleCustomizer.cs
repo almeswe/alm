5032d31c8ff23313733e44acd10e37f9559ad2d4
Almeswe <52577119+Almeswe@users.noreply.github.com>
2021-01-11 11:02:36 +0300

    Update ConsoleCustomizer.cs

--- alm/Alm.Other/Alm.Other.ConsoleStuff/ConsoleCustomizer.cs
+++ alm/Alm.Other/Alm.Other.ConsoleStuff/ConsoleCustomizer.cs
@@ -6,48 +6,6 @@ namespace alm.Other.ConsoleStuff
 {
     public sealed class ConsoleCustomizer
     {
-        private static ConsoleColor[] colors_copy = new ConsoleColor[]{
-        ConsoleColor.DarkBlue,
-        ConsoleColor.DarkGreen,
-        ConsoleColor.DarkCyan,
-        ConsoleColor.DarkRed,
-        ConsoleColor.DarkMagenta,
-        ConsoleColor.DarkYellow ,
-        ConsoleColor.Gray,
-        ConsoleColor.DarkGray,
-        ConsoleColor.Blue,
-        ConsoleColor.Green,
-        ConsoleColor.Cyan,
-        ConsoleColor.Red,
-        ConsoleColor.Magenta,
-        ConsoleColor.Yellow,
-        ConsoleColor.White
-        };
-        private static List<ConsoleColor> colors = colors_copy.ToList();
-        public static ConsoleColor currColor;
-        public static ConsoleColor GetColor()
-        {
-            /*Все узлы данного узла окрашиваются в один цвет*/
-            if (colors.Count > 0)
-            {
-                currColor = colors[0];
-                colors.RemoveAt(0);
-                return currColor;
-            }
-            else
-            {
-                colors = colors_copy.ToList();
-                currColor = colors[0];
-                colors.RemoveAt(0);
-                return currColor;
-            }
-        }
-        public static ConsoleColor GetColor(int level)
-        {
-            /* Все узлы одной вложенности окрашиваются в один цвет */
-            if (level >= colors_copy.Length) level -= (int)(level / colors_copy.Length) * colors_copy.Length;
-            return colors_copy[level];
-        }
         public static void ColorizedPrint(string message, ConsoleColor color = ConsoleColor.Gray)
         {
             Console.ForegroundColor = color;
@@ -60,6 +18,5 @@ namespace alm.Other.ConsoleStuff
             Console.WriteLine(message);
             Console.ResetColor();
         }
-
     }
 }
